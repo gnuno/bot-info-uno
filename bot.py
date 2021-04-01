63112f83d41b063611c54c9afbd7045a63d61724
Matias Gimenez <meganiunmati@hotmail.com>
2021-04-01 15:46:16 -0300

    print message to know

--- bot.py
+++ bot.py
@@ -34,8 +34,10 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['siu'])
 def request_siu_information(message):
+    print(message)
     user_id = message.from_user.id
     logger.info(f"El usuario {user_id} ha solicitado información del SIU.")
+    # bot.send_message()
     bot.reply_to(
         message, f"Solicitando información de https://autogestion.uno.edu.ar/uno/")
     siu_data = get_siu_info()
