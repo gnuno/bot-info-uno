456c2b93e74d88a7b506f06366498cf11bb8ff24
Matias Gimenez <meganiunmati@hotmail.com>
2021-04-01 14:57:51 -0300

    adding siu request

--- bot.py
+++ bot.py
@@ -2,8 +2,12 @@ import telebot
 from flask import Flask, request
 import os
 import logging
+import requests
+import time
 
 # Configurar logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
@@ -13,16 +17,30 @@ bot = telebot.TeleBot(API_TOKEN)
 server = Flask(__name__)
 
 
+def get_siu_info():
+    siu_data = dict()
+    t0 = time.time()
+    r = requests.get('https://autogestion.uno.edu.ar/uno/')
+    t1 = time.time()
+    siu_data['status'] = r.status_code
+    siu_data['latency'] = int((t1-t0) * 1000)
+    return siu_data
+
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Bienvenido a este bot de prueba")
 
 
 @bot.message_handler(commands=['siu'])
-def send_welcome(message):
+def request_siu_information(message):
     user_id = message.from_user.id
-    logger.debug(f"El usuario {user_id} ha solicitado información del SIU")
-    bot.reply_to(message, "Comando SIU")
+    logger.info(f"El usuario {user_id} ha solicitado información del SIU.")
+    bot.reply_to(
+        message, f"Solicitando información de https://autogestion.uno.edu.ar/uno/")
+    siu_data = get_siu_info()
+    bot.reply_to(
+        message, f"Siu Guaraní ha respondido con código {siu_data.status} en {siu_data.latency}ms")
 
 
 @server.route('/' + API_TOKEN, methods=['POST'])
