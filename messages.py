622f83cc5ca5e2be3fb8bed82ee75a81e0c3cdc0
Matias Gimenez <meganiunmati@hotmail.com>
2021-04-01 23:47:40 -0300

    erase string pluses

--- messages.py
+++ messages.py
@@ -7,8 +7,4 @@ def siu_failure_message(latency):
 
 
 def help_message():
-    return "<b>infoUNObot</b> te brinda información necesaria sobre la Universidad Nacional del Oeste, desde el estado del siu o el campus, hasta fechas importantes o links utiles.<br/>" +
-    "<b>/help</b> - Muestra este mensaje.\n<b>/siu</b> - Obtiene información del siu para saber el estado del mismo y su latencia.<br/>" +
-    "<b>/links</b> - Te devuelve un listado de links utiles sobre la carrera (grupos, comunidades, etc).<br/>" +
-    "<b>/calendar</b> - Te muestra las fechas importantes del calendario académico de la Universidad.<br/>" +
-    "<br/> Este bot fue posible y llevado a cabo gracias a GNUno, cualquier consulta o pregunta hacela aquí: <inserte contacto de gnuno>."
+    return f"<b>infoUNObot</b> te brinda información necesaria sobre la Universidad Nacional del Oeste, desde el estado del siu o el campus, hasta fechas importantes o links utiles.<br/><b>/help</b> - Muestra este mensaje.\n<b>/siu</b> - Obtiene información del siu para saber el estado del mismo y su latencia.<br/><b>/links</b> - Te devuelve un listado de links utiles sobre la carrera (grupos, comunidades, etc).<br/><b>/calendar</b> - Te muestra las fechas importantes del calendario académico de la Universidad.<br/><br/> Este bot fue posible y llevado a cabo gracias a GNUno, cualquier consulta o pregunta hacela aquí: <inserte contacto de gnuno>."
