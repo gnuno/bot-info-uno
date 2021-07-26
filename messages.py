901736ddc63f916d790cd969d4146577a7f891b4
Matias <matiasnoriega86@gmail.com>
2021-07-25 22:30:38 -0300

    (fix): actualiza mensaje de ayuda

--- messages.py
+++ messages.py
@@ -14,11 +14,11 @@ def url_failure_message(name):
 
 def help_message():
     message = f"<b>infoUNObot</b> te brinda información necesaria sobre la Universidad Nacional del Oeste, desde el estado del siu o el campus, hasta fechas importantes o links útiles.\n\n"
-    message += f"<b>/help</b> - Muestra este mensaje.\n"
+    message += f"<b>/help</b> ó <b>/ayuda</b> - Muestra este mensaje.\n"
     message += f"<b>/siu</b> - Obtiene información del siu para saber el estado del mismo y su latencia.\n"
     message += f"<b>/campus</b> - Obtiene información del campus virtual para saber el estado del mismo y su latencia.\n"
     message += f"<b>/links</b> - Te devuelve un listado de links útiles sobre la carrera (grupos, comunidades, etc).\n"
-    message += f"<b>/calendar</b> - Te muestra las fechas importantes del calendario académico de la Universidad.\n"
+    message += f"<b>/calendar</b> ó <b>/calendario</b> - Te muestra las fechas importantes del calendario académico de la Universidad. Si escribis <b>/calendar feriados</b> te muestra los feriados del año corriente\n"
     message += f"<b>/mails</b> - Te muestra los mails más importantes de las escuelas, además si especificás la escuela te filtra el resultado.\n"
     message += f"\nEste bot fue posible y llevado a cabo gracias a GNUno, cualquier consulta o pregunta hacela aquí: <i>https://t.me/gnuno_merlo</i>."
     return message
